1c47a8b39cdf2b306998bcf719ebb24abc769508
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-07 19:49:49 +0200

    add sumNumbersFromUser function with a callback and declare result variable

--- anonymus_function.js
+++ anonymus_function.js
@@ -1,10 +1,25 @@
-const fn = function(messageForUser){
+/* const fn = function(messageForUser){
     const userInput = prompt(messageForUser)
 
     return userInput
 }
 const inputFromUser = fn("ile masz lat?")
-console.log(inputFromUser)
+console.log(inputFromUser) */
 
 ////////////////////////////
 
+const getUserInput = function(message) {
+    return prompt(message)
+}
+
+const sumNumbersFromUser = function(userInputFn) {
+    const a = userInputFn("Podaj liczbę a")
+    const b = userInputFn("Podaj liczbę b")
+
+    const sum = parseFloat(a) + parseFloat(b)
+
+    return sum;
+}
+
+const result = sumNumbersFromUser(getUserInput)
+console.log(result)
