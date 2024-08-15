f4ad38110c74a6e141bfe89c10014ccffe01c472
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-15 16:40:54 +0200

    add few TImout functions to showcase their functionality

--- time_function.js
+++ time_function.js
@@ -1,1 +1,14 @@
+function showMessage() {
+    console.log("To jest pierwsza wiadomość")
+}
+
+function showName(name) {
+    console.log("twoje imię to " + name)
+}
+
+setTimeout(showMessage,1000)
+setTimeout(showName,2000)
+setTimeout(function(){
+    console.log("this is the anonymus function which will be displayed as last")
+}, 5000)
 
