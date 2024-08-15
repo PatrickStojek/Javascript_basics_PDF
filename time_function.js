28c1263950004ed29a162376cb08c799fd68e4e7
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-15 17:20:11 +0200

    showcase tge execution order with setInterval

--- time_function.js
+++ time_function.js
@@ -13,13 +13,24 @@ setTimeout(function(){
     console.log("this is the anonymus function which will be displayed as last")
 }, 5000)
 
-/* setINterval functions */
+/* setInterval functions */
 
-let counter = 0
+/* let counter = 0
 function showMessage(message) {
     setInterval(function() {
         counter++
         console.log(message + " " + counter)
     }, 3000)
 }
-showMessage("Ta wiadomość pokazuje ci się po raz")
+showMessage("Ta wiadomość pokazuje ci się po raz") */
+
+let counter = 1;
+
+const showCounter = function() {
+    console.log(counter)
+    counter++
+}
+
+console.log("Before setInterval") // 1
+setInterval(showCounter, 2000) // 3
+console.log("After setInterval") // 2
