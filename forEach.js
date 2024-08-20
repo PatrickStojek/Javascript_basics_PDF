54d6382a3da8cf89a1b6dcbfe29f918b215048dd
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-20 22:40:14 +0200

    add getSum function which is more efficient than using the standard for loop approach

--- forEach.js
+++ forEach.js
@@ -1,7 +1,17 @@
-const numbers = [1,2,3,4,5,6];
+const numbers = [1,2,10,4,5,6];
 
 function showItemwithIndex(item,index) {
     console.log(item + " --> " + index)
 }
-
 numbers.forEach(showItemwithIndex);
+
+function getSum(arr) {
+    let sum = 0;
+    arr.forEach(function(number) {
+        sum += number
+    })
+    return sum
+}
+
+console.log(getSum(numbers))
+
