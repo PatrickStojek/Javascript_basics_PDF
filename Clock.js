2356bea29aa99b589e587b760cecd39c27419b4f
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-21 23:28:55 +0200

    add clock program for displaying currect time in title of the tab and in console

---
+++ Clock.js
@@ -0,0 +1,19 @@
+setInterval(function() {
+    const now = new Date();
+    const time = getTime(now);
+
+    document.title = time;
+    console.clear();
+    console.log(time)
+}, 1000)
+
+function getTime(date) {
+    let time = startWithZero(date.getHours());
+    time += ':' + startWithZero(date.getMinutes())
+    time += ':' + startWithZero(date.getSeconds())
+    return time;
+}
+
+function startWithZero(num) {
+    return (num < 10 ? '0' : '' ) + num
+}
---
+++ embedObjects.js
@@ -0,0 +1,8 @@
+const now = new Date()
+
+console.log(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getHours(),
+)
+
