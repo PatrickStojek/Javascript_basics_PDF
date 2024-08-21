1f7f55a73d76d869e586ba3f42bf0a7c3a22b5a6
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-21 22:32:25 +0200

    creating object methods inside and outside the object

--- objects.js
+++ objects.js
@@ -2,9 +2,26 @@ const car = {
     color: 'red',
     milage: 1203,
     horsepower: 540,
-    seatsNumber: 2
+    seatsNumber: 2,
+    getColor: function() {
+        return car.color
+    },
 }
 
 car.color = 'blue'
 console.log(car.color, car['color']) 
+
+const hp = car.horsepower
+
+console.log(hp) // 540
+
+
+car.getSeatsNumber = function() {
+    return car.seatsNumber
+}
+
+console.log(
+    car.getColor(), //blue
+    car.getSeatsNumber() //2
+)
 
