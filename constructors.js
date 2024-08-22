f48bd20fccde9964ebdfc81ed2b67eb72469bbf9
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-22 23:22:59 +0200

    add few new methods to our Student constructor

--- constructors.js
+++ constructors.js
@@ -1,6 +1,10 @@
 function Student(initFirstName, initLastName) {
     this.firstName = initFirstName;
     this.LastName = initLastName;
+    this.class = '2B';
+    this.showFullName = function() {
+        console.log(this.firstName, this.LastName)
+    }
 }
 
 const student1 = new Student('Marek', 'Nowak')
@@ -10,3 +14,10 @@ console.log(
     student1, //{firstName: Marek, lastName: Nowak}
     student2, //{firstName: Anna, lastName: Kowal}
 )
+
+console.log(
+    student1.class, // 2B
+    student2.class // 2B
+)
+
+student1.showFullName // Marek , Nowak
